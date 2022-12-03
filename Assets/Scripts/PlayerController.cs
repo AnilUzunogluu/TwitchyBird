70f96043b9b7eb47395c39de4b95118370891e2e
Anıl Uzunoğlu <aniluzunoglu@hotmail.com>
2022-12-04 02:49:45 +0300

    Twitchy Bird v0.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,18 +5,27 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float upwardsForce;
     
+    [Header("Rotation Variables")]
+    [SerializeField] private float upwardsRotationAngle;
+    [SerializeField] private float rotationSpeed;
+    
     
     [SerializeField] private UnityEvent onTap;
     [SerializeField] private UnityEvent onPoint;
 
 
+    [SerializeField] private float velocity;
+
+
     private Rigidbody2D _rigidbody;
     private float _lastClickTime;
+    private GameObject _birdSprite;
 
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _birdSprite = transform.GetChild(0).gameObject;
     }
 
     private void Update()
@@ -35,6 +43,10 @@ public class PlayerController : MonoBehaviour
             
             _rigidbody.AddForce(Vector3.up * upwardsForce, ForceMode2D.Force);
         }
+        
+        SetRotation();
+
+        velocity = _rigidbody.velocity.y;
     }
 
     private float TimeBetweenTaps(float lastClick)
@@ -57,5 +69,39 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Dead!");
         }
+    }
+
+    private void SetRotation()
+    {
+        var yVelocity = _rigidbody.velocity.y;
+
+        if (yVelocity < 0)
+        {
+            FallRotation(yVelocity);
+        }
+        else
+        {
+            UpRotation();
+        }
+    }
+
+    private void FallRotation(float yVelocity)
+    {
+        var rotationAngle = yVelocity * 20;
+        
+        if (rotationAngle < -90)
+        {
+            rotationAngle = -90;
+        }
+        
+        var targetRotation = Quaternion.Euler(new Vector3(0f, 0f, rotationAngle));
+        var increasingRotationSpeed = Mathf.Abs((float) (yVelocity * 0.3 * rotationSpeed));
+        _birdSprite.transform.rotation = Quaternion.RotateTowards(_birdSprite.transform.rotation, targetRotation, increasingRotationSpeed * Time.deltaTime);
+    }
+
+    private void UpRotation()
+    {
+        var targetRotation = Quaternion.Euler(new Vector3(0f, 0f, upwardsRotationAngle));
+        _birdSprite.transform.rotation = Quaternion.RotateTowards(_birdSprite.transform.rotation, targetRotation, rotationSpeed * 5 * Time.deltaTime);
     }
 }
